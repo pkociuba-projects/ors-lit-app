497480097cdee07bc67db12a5354d66796da4532
pkociuba-projects <piotrkociuba@kul.pl>
2025-12-17 10:00:41 +0100

    Update API key in config.ts

--- src/ors-api/config.ts
+++ src/ors-api/config.ts
@@ -1,4 +1,4 @@
 export default {
-    apiKey: 'eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6ImVjYzViMWI4YmU3NzliZmQ1YjhiMGVlMDdiMzk1ODNiMzAwY2JhNGRhZjk5MDU1NWQ5ZmIxODI0IiwiaCI6Im11cm11cjY0In0=',
+    apiKey: 'your-api-key-here',
     geocodeServiceUrl: "https://api.openrouteservice.org/geocode/search?"
 }
